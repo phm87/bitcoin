4079dc5c8e3590b9956cbc55b36fe18a943d6133
phm87 <31578435+phm87@users.noreply.github.com>
2018-11-12 22:23:29 +0100

    Update rpcwallet.cpp

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -343,6 +343,7 @@ static UniValue sendtoaddress(const JSONRPCRequest& request)
 
     // WalletLogPrintf("sendtoaddress max_withdrawal_cent %d \n", max_withdrawal_cent);
     
+    extern int max_withdrawal_cent;
     throw JSONRPCError(RPC_WALLET_ERROR, strprintf("max_withdrawal_cent:: %d", max_withdrawal_cent));
     
     if (request.fHelp || request.params.size() < 2 || request.params.size() > 8)
@@ -776,7 +777,7 @@ static UniValue sendmany(const JSONRPCRequest& request)
     }
 
     
-    
+    extern int max_withdrawal_cent;
     // WalletLogPrintf("sendmany max_withdrawal_cent %d \n", max_withdrawal_cent);
     throw JSONRPCError(RPC_WALLET_ERROR, strprintf("max_withdrawal_cent:: %d", max_withdrawal_cent));
                    
