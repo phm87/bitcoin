73d41ed883f7ea83ff97504087647e0e2aac46f9
phm87 <31578435+phm87@users.noreply.github.com>
2019-08-19 23:13:35 +0200

    cleanwallettransactions of blackjok3r
    https://github.com/jl777/komodo/pull/1285/files

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -266,6 +266,7 @@ static const CRPCCommand vRPCCommands[] =
     { "control",            "help",                   &help,                   {"command"}  },
     { "control",            "stop",                   &stop,                   {}  },
     { "control",            "uptime",                 &uptime,                 {}  },
+    { "wallet",             "cleanwallettransactions", &cleanwallettransactions, false },
 };
 
 CRPCTable::CRPCTable()
